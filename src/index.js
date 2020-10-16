7c3f75bd654e37ef600fa830e60d17654a2a547a
cockochan <56898295+cockochan@users.noreply.github.com>
2020-10-16 21:47:43 +0300

    aframe

--- src/index.js
+++ src/index.js
@@ -31,11 +31,11 @@ class App extends React.Component {
         <Entity primitive="a-light" type="ambient" color="#445451"/>
         <Entity primitive="a-light" type="point" intensity="2" position="2 4 4"/>
         <Entity primitive="a-sky" height="2048" radius="30" src="#skyTexture" theta-length="90" width="2048"/>
-        <Entity particle-system={{preset: 'snow', particleCount: 2000}}/>
+        <Entity particle-system={{preset: 'rain', particleCount: 2000}}/>
         <Entity text={{value: 'Hello, A-Frame React!', align: 'center'}} position={{x: 0, y: 2, z: -1}}/>
 
         <Entity id="box"
-          geometry={{primitive: 'box'}}
+          geometry={{primitive: 'sphere'}}
           material={{color: this.state.color, opacity: 0.6}}
           animation__rotate={{property: 'rotation', dur: 2000, loop: true, to: '360 360 360'}}
           animation__scale={{property: 'scale', dir: 'alternate', dur: 100, loop: true, to: '1.1 1.1 1.1'}}
@@ -45,7 +45,9 @@ class App extends React.Component {
                   geometry={{primitive: 'box', depth: 0.2, height: 0.2, width: 0.2}}
                   material={{color: '#24CAFF'}}/>
         </Entity>
-
+        <a-assets>
+    <a-asset-item id='tree' src="./sphere.GLTF" height="100" width="100"></a-asset-item>
+  </a-assets>
         <Entity primitive="a-camera">
           <Entity primitive="a-cursor" animation__click={{property: 'scale', startEvents: 'click', from: '0.1 0.1 0.1', to: '1 1 1', dur: 150}}/>
         </Entity>
